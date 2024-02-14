15fa16a475899e6895fddfd158faacc73c17f1e0
Manoj Baba <dmanojbaba@gmail.com>
2024-02-14 16:36:36 +0000

    add prom

--- build-images.sh
+++ build-images.sh
@@ -1,2 +1,3 @@
 docker build -t hello-backend:latest -f ./hello-backend/Dockerfile.local ./hello-backend/
 docker build -t hello-frontend:latest -f ./hello-frontend/Dockerfile.local ./hello-frontend/
+docker build -t hello-prometheus:latest ./hello-prometheus/
--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 docker build -t hello-backend:latest ./hello-backend/
 docker build -t hello-frontend:latest ./hello-frontend/
+docker build -t hello-prometheus:latest ./hello-prometheus/
