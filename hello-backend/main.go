76faa7753e104a13d7ae77cf21d616eda193bd51
Manoj Baba <dmanojbaba@gmail.com>
2024-02-19 17:03:33 +0000

    updates

--- hello-backend/main.go
+++ hello-backend/main.go
@@ -8,23 +8,26 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprintf(w, "Hello, World!")
+	} else {
+		fmt.Fprintf(w, "Hello, %s!", name)
+	}
+}
+
 func main() {
+	fmt.Println("Server listening on port 9002")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello Backend")
 	})
 
 	http.Handle("/prometheus", promhttp.Handler())
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-		if name == "" {
-			fmt.Fprintf(w, "Hello, World!")
-		} else {
-			fmt.Fprintf(w, "Hello, %s!", name)
-		}
-	})
+	http.HandleFunc("/hello", hello)
 
-	fmt.Println("Server listening on port 9002")
 	if err := http.ListenAndServe(":9002", nil); err != nil {
 		log.Fatal(err)
 	}
